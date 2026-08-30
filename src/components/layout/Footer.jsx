1a5d0960d75561e6699ff68b7458982834564447
MadoScientista <samuel.cortes.n@gmail.com>
2026-08-30 17:24:15 -0400

    cambio de atributo class a className

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,8 +4,8 @@ export function Footer(){
     return (
         <div className="container-fluid bg-dark text-light" style={{minHeight:"10rem"}}>
             <div className="container pt-5">
-                <i class="bi bi-credit-card-2-back"></i>
-                <i class="bi bi-credit-card"></i>
+                <i className="bi bi-credit-card-2-back"></i>
+                <i className="bi bi-credit-card"></i>
                 <p>Hola</p>
             </div>
             
