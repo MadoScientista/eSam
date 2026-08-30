d48b3d277a8c8ab5d5588a6759e3074a4981e7ee
MadoScientista <samuel.cortes.n@gmail.com>
2026-08-30 17:23:50 -0400

    Validación de campos mediante funciones

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,17 +1,77 @@
-
+import { useState } from "react"
 
 export function LoginForm(){
+    
+    const [ formulario, setFormulario ] = useState({
+        email: "",
+        password: ""
+    })
+
+    const handleChange = (e)=>{
+
+        // Almacena en un objeto el nombre
+        // y valor del objetivo del evento
+        const {name, value} = e.target
+
+        // Copia el objeto formulario actual
+        // y modifica el campo objetivo
+        setFormulario({
+            ...formulario,
+            [name]: value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if(validarFormulario()){
+            console.log("Sesión iniciada")
+        }else{
+            console.log("Error al iniciar sesión")
+        }
+
+    }
+
+    const validarFormulario = () =>{
+        
+        // Validación dominio del correo
+        const dominiosValidos = ["duoc.cl", "profesor.duoc.cl", "gmail.com"]
+        const dominio = formulario.email.split("@")[1]
+
+        if(!dominiosValidos.includes(dominio)){
+            console.log("Dominio inválido")
+            return false
+        }
+
+        return true
+    }
+
     return (
         <div className="login-form border p-5 rounded-2 mb-5">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="h3">Inicio de Sesión</div>
                 <div className="mb-3">
-                    <label for="exampleInputEmail1" className="form-label">Correo</label>
-                    <input type="email" className="form-control" id="userEmail" aria-describedby="emailHelp"/>
+                    <label htmlFor="email" className="form-label">Correo</label>
+                    <input 
+                        type="email" 
+                        className="form-control" 
+                        name="email" 
+                        maxLength={100} // Longitud máxma 100 caracteres
+                        required        // Correo requerido
+                        value={formulario.email} 
+                        onChange={handleChange}/>
                 </div>
                 <div className="mb-3">
-                    <label for="exampleInputPassword1" className="form-label">Contraseña</label>
-                    <input type="password" className="form-control" id="userPassword"/>
+                    <label htmlFor="exampleInputPassword1" className="form-label">Contraseña</label>
+                    <input 
+                        type="password" 
+                        className="form-control" 
+                        name="password" 
+                        minLength={4}   // Longitud mínima 4 caracteres
+                        maxLength={10}  // Longitud máxima 10 caracteres
+                        required        // Contraseña requerida
+                        value={formulario.password}
+                        onChange={handleChange}/>
                 </div>
                 <button type="submit" className="btn btn-dark">Entrar</button>
             </form>
--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,52 +1,126 @@
+import { useState } from "react"
+import { regionesComunas } from "../const/regionesComunas"
 
 
 export function RegisterForm(){
+    const [formulario, setFormulario] = useState({
+        nombre:"",
+        email:"",
+        emailConfirm:"",
+        password:"",
+        passwordConfirm:"",
+        telefono:"",
+        selectRegion:"", 
+        selectComuna:"",
+    })
+
+    const [comunas, setComunas] = useState([])
+
+    const handleChange = (e)=>{
+        const {name, value} = e.target
+
+        setFormulario({
+            ...formulario,
+            [name]:value
+        })
+
+        if(name === "selectRegion"){
+            if(value != ""){
+                const listaComunas = regionesComunas[value]["comunas"]
+                setComunas(
+                    listaComunas.map((c,i)=>{
+                        return <option value={i} key={`c${i}`}>{c}</option>
+                    })
+                )
+            }
+        }
+    }
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        console.log(formulario)
+    }
+
+
     return(
         <div className="register-form border p-5 rounded-2">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="h3 mb-5 text-center">Formulario de Registro</div>
                 <div className="mb-3">
-                    <label for="exampleInputEmail1" className="form-label">Nombre completo</label>
-                    <input type="text" className="form-control" id="userFullName" aria-describedby="emailHelp"/>
+                    <label htmlFor="nombre" className="form-label">Nombre completo</label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        name="nombre"
+                        maxLength={100} 
+                        onChange={handleChange}/>
                 </div>
                 <div className="mb-3">
-                    <label for="exampleInputEmail1" className="form-label">Correo</label>
-                    <input type="email" className="form-control" id="userEmail" aria-describedby="emailHelp"/>
+                    <label htmlFor="email" className="form-label">Correo</label>
+                    <input 
+                        type="email" 
+                        className="form-control" 
+                        name="email" 
+                        maxLength={100}
+                        onChange={handleChange}/>
                 </div>
                 <div className="mb-3">
-                    <label for="exampleInputEmail1" className="form-label">Confirme correo</label>
-                    <input type="email" className="form-control" id="userEmailConfirm" aria-describedby="emailHelp"/>
+                    <label htmlFor="emailConfirm" className="form-label">Confirme correo</label>
+                    <input 
+                        type="email" 
+                        className="form-control" 
+                        name="emailConfirm"
+                        maxLength={100} 
+                        onChange={handleChange}/>
                 </div>
                 <div className="mb-3">
-                    <label for="exampleInputPassword1" className="form-label">Contraseña</label>
-                    <input type="password" className="form-control" id="userPassword"/>
+                    <label htmlFor="password" className="form-label">Contraseña</label>
+                    <input 
+                        type="password" 
+                        className="form-control" 
+                        name="password"
+                        minLength={4}
+                        maxLength={10}
+                        onChange={handleChange}/>
                 </div>
                 <div className="mb-3">
-                    <label for="exampleInputPassword1" className="form-label">Confirme Contraseña</label>
-                    <input type="password" className="form-control" id="userPasswordConfirm"/>
+                    <label htmlFor="passwordConfirm" className="form-label">Confirme Contraseña</label>
+                    <input 
+                        type="password" 
+                        className="form-control" 
+                        name="passwordConfirm"
+                        minLength={4}
+                        maxLength={10}
+                        onChange={handleChange}/>
                 </div>
                 <div className="mb-3">
-                    <label for="exampleInputPassword1" className="form-label">Teléfono</label>
-                    <input type="text" className="form-control" id="userPasswordConfirm"/>
+                    <label htmlFor="telefono" className="form-label">Teléfono</label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        name="telefono"
+                        maxLength={9}
+                        onChange={handleChange}/>
                 </div>
                 <div className="mb-3">
                     <div className="row">
                         <div className="col">
                             <label htmlFor="selectRegion" className="form-label">Region</label>
-                            <select id="selectRegion" class="form-select" aria-label="Default select example">
-                                <option selected value="0">-- Seleccione Región --</option>
-                                <option value="1">Región Metropolitana</option>
-                                <option value="2">Atacama</option>
-                                <option value="3">Antofagasta</option>
+                            <select name="selectRegion" className="form-select" onChange={handleChange}>
+                                <option value="">-- Seleccione Región --</option>
+                                {
+                                    regionesComunas.map((r, i)=>{
+                                        return <option value={i} key={`r${i}`}>{r.region}</option>
+                                    })
+                                    
+                                }
                             </select>
                         </div>
                         <div className="col">
                             <label htmlFor="selectComuna" className="form-label">Comuna</label>
-                            <select id="selectRegion" class="form-select" aria-label="Default select example">
-                                <option selected value="0">-- Seleccione Comuna --</option>
-                                <option value="1">Región Metropolitana</option>
-                                <option value="2">Atacama</option>
-                                <option value="3">Antofagasta</option>
+                            <select name="selectComuna" className="form-select" onChange={handleChange}>
+                                <option value="">-- Seleccione Comuna --</option>
+                                {comunas}
                             </select>
                         </div>
                     </div>
