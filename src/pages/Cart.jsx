f3df765dd331b773462f2e05e6fb477f3f635b3c
MadoScientista <samuel.cortes.n@gmail.com>
2026-08-31 11:08:40 -0400

    Agrega sección de productos relacionados

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
 import { ProductCardH } from "../components/ProductCardH"
+import { ProductList } from "../components/ProductList"
 import { cartProducts } from "../const/cartProducts"
+import { products } from "../const/products"
 
 export function Cart(){
 
@@ -30,26 +32,32 @@ export function Cart(){
 
     return (
         <div className="container pt-5">
-            <h2 className="mb-5">Carrito de compras</h2>
-            <div className="row">
-                <div className="col border-end me-5">
-                    {cartProducts.map((p)=>{
-                        return <div className="row"><ProductCardH product={p}/></div>
-                    })}
+            <div className="container mb-5" style={{minHeight:"60vh"}}>
+                <h2 className="mb-5">Carrito de compras</h2>
+                <div className="row">
+                    <div className="col border-end me-5">
+                        {cartProducts.map((p)=>{
+                            return <div className="row"><ProductCardH product={p}/></div>
+                        })}
+                    </div>
+                    <div className="col">
+                        <h3>Resumen</h3>
+                        <hr />
+                        <p>Productos en el carro: {nProductos}</p>
+                        {
+                            cartProducts.map((p)=>{
+                                return <p>{p.cantidad} x {p.nombre}</p>
+                            })
+                        }
+                        <hr />
+                        <p>Sub total: ${subtotal}</p>
+                        <button className="btn btn-dark">Pagar</button>
+                    </div>
                 </div>
-                <div className="col">
-                    <h3>Resumen</h3>
-                    <hr />
-                    <p>Productos en el carro: {nProductos}</p>
-                    {
-                        cartProducts.map((p)=>{
-                            return <p>{p.cantidad} x {p.nombre}</p>
-                        })
-                    }
-                    <hr />
-                    <p>Sub total: ${subtotal}</p>
-                    <button className="btn btn-dark">Pagar</button>
-                </div>
+            </div>
+            <h3 className="mb-5">Productos relacionados</h3>
+            <div className="container p-0">
+                <ProductList products={products.slice(0,5)} cols={5}/>
             </div>
         </div>
     )
