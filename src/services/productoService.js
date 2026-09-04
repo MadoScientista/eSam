0493e1683571d1e27668f566b694fe403ba6176c
MadoScientista <samuel.cortes.n@gmail.com>
2026-09-03 20:24:34 -0400

    Nuevos métodos post para crear y actualizar productos

--- src/services/productoService.js
+++ src/services/productoService.js
@@ -1,14 +1,33 @@
 import {api} from "./api"
 
 
+// Obtener todos los productos
 export const obtenerProductos = async () => {
     const response = await api.get("/productos")
 
     return response.data
 }
 
+
+// Obtener un producto filtrado por sku
 export const obtenerProductoSku = async (sku) => {
     const response = await api.get("/productos/" + sku)
+
+    return response.data
+}
+
+
+// Post para guardar un producto
+export const crearProductoSku = async (producto) => {
+    const response = await api.post("/productos/", producto)
+
+    return response.data
+}
+
+
+// Post para actualizar un producto
+export const actualizarProductoSku = async (sku, producto) => {
+    const response = await api.post("/productos/" + sku, producto)
 
     return response.data
 }
